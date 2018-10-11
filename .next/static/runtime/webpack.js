c34778c1f39b1f953ca8b0eacd9e5ac969d89b2a
Creighton <creighton@creightonsoukup.com>
2018-10-11 11:35:36 -0400

    rotating gallery

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "524045e58ac8347d1d18";
+/******/ 	var hotCurrentHash = "e78db9856ec4b5ad77a3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/rotating-gallery.js
+++ components/rotating-gallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Slider from 'react-slider'
+
 
 export default class RotatingGallery extends React.Component {
   constructor(props) {
@@ -10,18 +10,10 @@ export default class RotatingGallery extends React.Component {
   }
 
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
 
-    }
     return (
       <div>
-        <Slider {...settings}>
-          
-        </Slider>
+
       </div>
     )
   }
