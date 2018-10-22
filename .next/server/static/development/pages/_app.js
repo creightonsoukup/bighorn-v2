11fc8c8e7c14bdf6a49567f428e45b299bf5b390
creightonsoukup <creighton@creightonsoukup.com>
2018-10-22 10:42:04 -0600

    updated nextjs

--- .next/server/static/development/pages/_app.js
+++ .next/server/static/development/pages/_app.js
@@ -112,7 +112,7 @@ module.exports = __webpack_require__(/*! next/app */ "next/app")
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/user/code/bighorn/bighorn-v2/node_modules/next/dist/pages/_app.js */"./node_modules/next/dist/pages/_app.js");
+module.exports = __webpack_require__(/*! /Users/creightonsoukup/code/bighorn-v2/node_modules/next/dist/pages/_app.js */"./node_modules/next/dist/pages/_app.js");
 
 
 /***/ }),
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e78db9856ec4b5ad77a3";
+/******/ 	var hotCurrentHash = "85750a699db8a25756f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
